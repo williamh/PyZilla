99d658832c1315f6224f7589300d6877ecc12d40
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-05-04 23:27:02 +0530

    Added URL

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1.0',
     author='Noufal Ibrahim',
     author_email='noufal@nibrahim.net.in',
+    url='http://github.com/nibrahim/PyZilla',
     py_modules = ['pyzilla'],
     license='LICENSE.txt',
     description='Useful towel-related stuff.',
