c6835f8db596ade636e24454008c2d644eea4401
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-05-06 00:06:13 +0530

    Added classifiers and fixed description

--- setup.py
+++ setup.py
@@ -8,6 +8,18 @@ setup(
     url='http://github.com/nibrahim/PyZilla',
     py_modules = ['pyzilla'],
     license='LICENSE.txt',
-    description='Useful towel-related stuff.',
+    description='Python wrapper for the BugZilla XML-RPC API',
     long_description=open('README.txt').read(),
+    classifiers=[
+    'Development Status :: 4 - Beta'
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)'
+    'Operating System :: POSIX',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: MacOS :: MacOS X',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+    'Topic :: Software Development :: Quality Assurance',
+    'Topic :: Utilities',
+    'Programming Language :: Python'
+    ]
 )
